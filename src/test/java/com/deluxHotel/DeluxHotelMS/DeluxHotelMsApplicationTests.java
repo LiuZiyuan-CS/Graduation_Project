9ff3898542f443d1cfa8f093aee0e58b4c58da40
LiuZiyuan-CS <497946860@qq.com>
2021-03-08 19:44:22 +0800

    第一次提交

---
+++ src/test/java/com/deluxHotel/DeluxHotelMS/DeluxHotelMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deluxHotel.DeluxHotelMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeluxHotelMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
